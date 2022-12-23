c3c0ba18750799872ea1704a5e6938481c80a02c
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2022-12-23 01:55:34 -0600

    prep for PyPi

---
+++ bruhcolor/__init__.py
@@ -0,0 +1,12 @@
+from bruhcolor import ATTRIBUTES, COLORS, HIGHLIGHTS, RESET, bruhcolored, bruhcolorwrapper, colors, color_codes
+
+__all__ = [
+    "ATTRIBUTES",
+    "COLORS",
+    "HIGHLIGHTS",
+    "RESET",
+    "bruhcolored",
+    "bruhcolorwrapper",
+    "colors",
+    "color_codes"
+]
