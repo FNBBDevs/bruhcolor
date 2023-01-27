249ddf5fddf072f9ab411010902e5ecfd840d1f4
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-01-26 23:25:25 -0600

    Thoooony >:(

---
+++ bruhcolor/testing.py
@@ -0,0 +1,8 @@
+from bruhcolor import bruhcolored8, colors8, color_codes8
+
+test = bruhcolored8("Hello World!", color="red", on_color="cyan", attrs=["blink", "crossed-out", "italic", "reverse"])
+
+print(test)
+
+print(len(test))
+print(len("Hello World!"))
